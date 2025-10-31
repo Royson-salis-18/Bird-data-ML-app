39a8997dd5596bd98d07398df45ff29d793140c4
Royson salis <143164082+Royson-salis-18@users.noreply.github.com>
2025-10-31 11:16:46 +0530

    Fix slider parameters for body mass input

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,6 @@ with st.sidebar:
   bill_length_mm = st.slider("bill lenght in mm", df.bill_length_mm.min(),df.bill_length_mm.max(),df.bill_length_mm.mean())
   bill_depth_mm = st.slider("bill depth in mm", df.bill_depth_mm.min(),df.bill_depth_mm.max(),df.bill_depth_mm.mean())
   flipper_length_mm = st.slider("flipper lenght in mm", float(df.flipper_length_mm.min()),float(df.flipper_length_mm.max()),float(df.flipper_length_mm.mean()))
-  body_mass_g = st.slider("body mass in g", df.body_mass_g.min(),df.body_mass_g.max(),df.body_mass_g.mean())
+  body_mass_g = st.slider("body mass in g", min_Value = float(df.body_mass_g.min()),max_value = float(df.body_mass_g.max()),value = float(df.body_mass_g.mean()))
 
 
