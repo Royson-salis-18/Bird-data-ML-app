f538f7522dabb06c4907794d48d641c9d30b83bf
Royson salis <143164082+Royson-salis-18@users.noreply.github.com>
2025-10-31 10:34:58 +0530

    Implement data expander for raw data display
    Added data expander to display raw penguin data and features.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.title('ML visualization app')
 
 st.write('Penguin Species prediction model')
+with st.expander('Data"):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  x
