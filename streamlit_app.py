5c4caa504b299d2f21793f261df763368dc22d54
Royson salis <143164082+Royson-salis-18@users.noreply.github.com>
2025-11-01 21:21:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ target_mapper = {'Adelie':0,
 def target_encode(val):
   return target_mapper[val]
 
-y = y_raw.Apply(target_encode)
+y = y_raw.apply(target_encode)
 y
 
 with st.expander('data preparation'):
