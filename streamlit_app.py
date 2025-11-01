ffa60908c525a5717337aeb0cf9516a7c573d0dc
Royson salis <143164082+Royson-salis-18@users.noreply.github.com>
2025-11-01 12:08:41 +0530

    Fix concatenation of input DataFrame with X

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ with st.sidebar:
           'sex':sex
         }
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df,X],axis=0)
+  input_penguins = pd.concat([input_df.X],axis=0)
 
 #encode x
 encode = ['island','sex']
